5db5a34277f7ccda2b6b2fcf4315e351fdb56068
tcyu <yutc93@qq.com>
2019-06-04 19:23:28 +0800

    修改了一些东西

--- src/main/java/com/team/manage/redis/RedisDemo.java
+++ src/main/java/com/team/manage/redis/RedisDemo.java
@@ -1,7 +1,10 @@
 package com.team.manage.redis;
 
+import com.alibaba.fastjson.JSONObject;
+import com.team.manage.entity.User;
 import redis.clients.jedis.Jedis;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static redis.clients.jedis.Protocol.Command.LRANGE;
@@ -18,19 +21,30 @@ public class RedisDemo {
         System.out.println("连接本地的 Redis 服务成功！");
         // 查看服务是否运行
          System.out.println("服务 正在运行: " + jedis.ping());
-         jedis.set("zhang","张三");
-         String a = jedis.get("zhang");
-//        System.out.println(a);
+         List<User> userList = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            User user = new User();
+            user.setAccount("aaa" + i);
+            user.setUserName("zzz" + i);
+            userList.add(user);
 
+        }
+
+//        System.out.println(a);
+        //清空list中的值
+        jedis.ltrim("list",1,0);
         //存入一个list
-        jedis.lpush("list",new String[]{"张三","李四","万物"});
-        List<String> list = jedis.lrange("list", 0, 2);
+        jedis.set("userList", JSONObject.toJSONString(userList));
+        String s1 = jedis.get("userList");
+        List<User> users = JSONObject.parseArray(s1,User.class);
+        System.out.println(s1);
+        List<String> list = jedis.lrange("list", 0, -1);
 //        String list1 = jedis.lpop("list");
 //        System.out.println(list1);
 
 
-        for (String s : list) {
-            System.out.println(s);
-        }
+//        for (String s : list) {
+//            System.out.println(s);
+//        }
     }
 }
